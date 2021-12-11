d84dc84bcf6fbe52d830435253da764235bbeae8
Rajnish <rockstarrajnish4658@gmail.com>
2021-12-11 22:20:02 +0530

    saving to github

--- index.js
+++ index.js
@@ -1,29 +1,457 @@
 function answer() {
-    const output= document.querySelector("#option1").value;
-    if(output=="A"){
-        document.querySelector("#para1").innerHTML="Correct answer; well done!";
+    let a= document.querySelector("#option1").value;
+    if(a=="A"){
+        document.querySelector("#para1").innerHTML="Correct answer; well done!"
     }
     else{
-        document.querySelector("#para1").innerHTML="Incorrect answer; try again!";
+        document.querySelector("#para1").innerHTML="Incorrect answer; well done!"
     }
 }
 
 function answer() {
-    const output= document.querySelector("#option2").value;
-    if(output=="A"){
-        document.querySelector("#para2").innerHTML="Correct answer; well done!";
+    let a= document.querySelector("#option2").value;
+    if(a=="A"){
+        document.querySelector("#para2").innerHTML="Correct answer; well done!"
     }
     else{
-        document.querySelector("#para2").innerHTML="Incorrect answer; try again!";
+        document.querySelector("#para2").innerHTML="Incorrect answer; well done!"
     }
 }
 
 function answer() {
-    const output= document.querySelector("#option3").value;
-    if(output=="A"){
-        document.querySelector("#para3").innerHTML="Correct answer; well done!";
+    let a= document.querySelector("#option3").value;
+    if(a=="A"){
+        document.querySelector("#para3").innerHTML="Correct answer; well done!"
     }
     else{
-        document.querySelector("#para3").innerHTML="Incorrect answer; try again!";
+        document.querySelector("#para3").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option4").value;
+    if(a=="D"){
+        document.querySelector("#para4").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para4").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option5").value;
+    if(a=="D"){
+        document.querySelector("#para5").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para5").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option6").value;
+    if(a=="B"){
+        document.querySelector("#para6").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para6").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option7").value;
+    if(a=="A"){
+        document.querySelector("#para7").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para7").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option8").value;
+    if(a=="A"){
+        document.querySelector("#para8").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para8").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option9").value;
+    if(a=="B"){
+        document.querySelector("#para9").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para9").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option10").value;
+    if(a=="A"){
+        document.querySelector("#para10").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para10").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option11").value;
+    if(a=="D"){
+        document.querySelector("#para11").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para11").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option12").value;
+    if(a=="A"){
+        document.querySelector("#para12").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para12").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option12").value;
+    if(a=="A"){
+        document.querySelector("#para12").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para12").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option13").value;
+    if(a=="C"){
+        document.querySelector("#para13").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para13").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option14").value;
+    if(a=="B"){
+        document.querySelector("#para14").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para14").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option15").value;
+    if(a=="B"){
+        document.querySelector("#para15").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para15").innerHTML="Incorrect answer; well done!"
+    }
+}
+function answer() {
+    let a= document.querySelector("#option16").value;
+    if(a=="C"){
+        document.querySelector("#para16").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para16").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option17").value;
+    if(a=="B"){
+        document.querySelector("#para17").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para17").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option18").value;
+    if(a=="D"){
+        document.querySelector("#para18").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para18").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option19").value;
+    if(a=="A"){
+        document.querySelector("#para19").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para19").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option20").value;
+    if(a=="D"){
+        document.querySelector("#para20").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para20").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option21").value;
+    if(a=="C"){
+        document.querySelector("#para21").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para21").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option22").value;
+    if(a=="B"){
+        document.querySelector("#para22").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para22").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option23").value;
+    if(a=="B"){
+        document.querySelector("#para23").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para23").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option24").value;
+    if(a=="D"){
+        document.querySelector("#para24").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para24").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option25").value;
+    if(a=="D"){
+        document.querySelector("#para25").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para25").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option26").value;
+    if(a=="A"){
+        document.querySelector("#para26").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para26").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option27").value;
+    if(a=="B"){
+        document.querySelector("#para27").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para27").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option28").value;
+    if(a=="D"){
+        document.querySelector("#para28").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para28").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option29").value;
+    if(a=="D"){
+        document.querySelector("#para29").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para29").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option30").value;
+    if(a=="D"){
+        document.querySelector("#para30").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para30").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option31").value;
+    if(a=="D"){
+        document.querySelector("#para31").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para31").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option32").value;
+    if(a=="A"){
+        document.querySelector("#para32").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para32").innerHTML="Incorrect answer; well done!"
+    }
+}
+function answer() {
+    let a= document.querySelector("#option33").value;
+    if(a=="D"){
+        document.querySelector("#para33").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para33").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option34").value;
+    if(a=="B"){
+        document.querySelector("#para34").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para34").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option35").value;
+    if(a=="B"){
+        document.querySelector("#para35").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para35").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option36").value;
+    if(a=="D"){
+        document.querySelector("#para36").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para36").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option37").value;
+    if(a=="B"){
+        document.querySelector("#para37").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para37").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option38").value;
+    if(a=="C"){
+        document.querySelector("#para38").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para38").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option39").value;
+    if(a=="C"){
+        document.querySelector("#para39").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para39").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option40").value;
+    if(a=="A"){
+        document.querySelector("#para40").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para40").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option41").value;
+    if(a=="B"){
+        document.querySelector("#para41").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para41").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option42").value;
+    if(a=="D"){
+        document.querySelector("#para42").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para42").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option43").value;
+    if(a=="B"){
+        document.querySelector("#para43").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para43").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option1").value;
+    if(a=="D"){
+        document.querySelector("#para44").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para44").innerHTML="Incorrect answer; well done!"
+    }
+}
+
+function answer() {
+    let a= document.querySelector("#option45").value;
+    if(a=="C"){
+        document.querySelector("#para45").innerHTML="Correct answer; well done!"
+    }
+    else{
+        document.querySelector("#para45").innerHTML="Incorrect answer; well done!"
     }
 }
