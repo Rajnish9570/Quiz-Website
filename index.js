0e7cdf7758ae4dd5d69e389358141fec430c0981
Rajnish <rockstarrajnish4658@gmail.com>
2021-12-11 20:23:38 +0530

    saving to github

---
+++ index.js
@@ -0,0 +1,29 @@
+function answer() {
+    const output= document.querySelector("#option1").value;
+    if(output=="A"){
+        document.querySelector("#para1").innerHTML="Correct answer; well done!";
+    }
+    else{
+        document.querySelector("#para1").innerHTML="Incorrect answer; try again!";
+    }
+}
+
+function answer() {
+    const output= document.querySelector("#option2").value;
+    if(output=="A"){
+        document.querySelector("#para2").innerHTML="Correct answer; well done!";
+    }
+    else{
+        document.querySelector("#para2").innerHTML="Incorrect answer; try again!";
+    }
+}
+
+function answer() {
+    const output= document.querySelector("#option3").value;
+    if(output=="A"){
+        document.querySelector("#para3").innerHTML="Correct answer; well done!";
+    }
+    else{
+        document.querySelector("#para3").innerHTML="Incorrect answer; try again!";
+    }
+}
